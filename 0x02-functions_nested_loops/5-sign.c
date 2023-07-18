55e81ba01d088d34cc0c069e13cbd778895954e8
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-18 13:19:50 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * print_sign - Entry point
+ * @c: describe the parameter
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+	putchar('+');
+	putchar(',');
+	return (1);
+}
+else if (n = 0)
+{
+	putchar('0');
+	putchar(',');
+	return (0);
+}
+else 
+{
+putchar('-');
+putchar(',');
+return (-1);
+}
+
+}
