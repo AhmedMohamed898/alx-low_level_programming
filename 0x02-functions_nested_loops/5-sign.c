7a959c81838c23cc3fb764fb9a0c6de18bfffe22
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-18 13:22:05 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,6 @@ int print_sign(int n)
 if (n > 0)
 {
 	putchar('+');
-	putchar(',');
 	return (1);
 }
 else if (n == 0)
@@ -25,7 +24,6 @@ else if (n == 0)
 else 
 {
 putchar('-');
-putchar(',');
 return (-1);
 }
 
