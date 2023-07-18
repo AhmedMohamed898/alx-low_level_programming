c0d166ddcb13141124249e429634eb988b40407e
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-18 12:38:17 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for( i = 0; i < 10; i++)
+	{
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void _ptchar(void);
 void print_alphabet(void);
+void print_alphabet_x10(void);
