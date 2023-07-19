f84c792d62f37fa255247a83db599ac0378e7f35
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-19 14:36:50 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,38 +10,22 @@
  */
 void jack_bauer(void)
 {
-int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
-
-while (h2 < 3)
+int a;
+int b;
+a = 0;
+while (a <= 59)
 {
-if (h1 == 3 && h2 ==2)
+b = 0;
+while (b <= 59)
 {
-break;
-}
-while (h1 < 10)
-{
-while (m2 < 6)
-{
-while (m1 < 10)
-{
-_putchar(h2 + '0');
-_putchar(h1 + '0');
-_putchar(':');
-_putchar(m2 + '0');
-_putchar(m1 + '0');
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+_puchar(':');
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
 _putchar('\n');
-m1++;
+b++;
 }
-m2++;
-m1 = 0;
-}
-h1++;
-m1 = 0;
-m2 = 0;
-}
-m1 = 0;
-m2 = 0;
-h2++;
-h1 = 0;
+a++
 }
 }
