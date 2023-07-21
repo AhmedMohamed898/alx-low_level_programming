601dec884c0c210570c9d86bd0f59bb425eb5fbd
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-21 15:43:02 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i < 101; i++)
+{
+if ((i % 3) == 0 && (i % 5) == 0)
+{
+printf("%s", "FizzBuzz");
+}
+else if ((i % 3) == 0)
+{
+printf("%s", "Fizz");
+}
+else if ((i % 5) == 0)
+{
+printf("%s", "Buzz");
+}
+else
+{
+printf("%d", i);
+}
+putchar(' ');
+}
+return (0);
+}
+
