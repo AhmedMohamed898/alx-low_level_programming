1fe3b8e2961231d8122518f35815c012c1c554cf
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-21 14:38:31 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-void more_numbers(void)
+int main(void)
 {
 int i;
 int j;
@@ -14,9 +14,15 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
 {
-_putchar('0' + j);
+if ((j / 10) > 0)
+{
+_putchar((j / 10) + '0');
+}
+_putchar('0' + (j % 10));
 }
 _putchar('\n');
 j = 0;
 }
+return (0);
 }
+
