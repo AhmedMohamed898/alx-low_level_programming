3903e7a0f84726da18417b7430642ed745520f0a
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-21 13:43:50 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * mul - Entry point
+ *@a: parameter
+ *@b: paramter
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
