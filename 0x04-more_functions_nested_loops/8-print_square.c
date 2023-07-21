0c53433a9ef2fa270ce73cf6c0f1a27d077976ba
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-21 15:19:07 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,12 @@ void print_square(int size)
 int i;
 int j;
 
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
@@ -17,5 +23,6 @@ for (j = 0; j < size; j++)
 putchar('#');
 }
 putchar('\n');
+}
 }
 }
