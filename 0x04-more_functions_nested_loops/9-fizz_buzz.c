84eae0a8d4d3ba1f4f5118c98b95558d30d6a2b1
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-21 15:55:28 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,30 @@ int main(void)
 int i;
 for (i = 1; i < 101; i++)
 {
-if ((i % 3) == 0 && (i % 5) == 0)
+if (i == 100)
+{
+printf("%s", "Buzz");
+}
+else if ((i % 3) == 0 && (i % 5) == 0)
 {
 printf("%s", "FizzBuzz");
+putchar(' ');
 }
 else if ((i % 3) == 0)
 {
 printf("%s", "Fizz");
+putchar(' ');
 }
 else if ((i % 5) == 0)
 {
 printf("%s", "Buzz");
+putchar(' ');
 }
 else
 {
 printf("%d", i);
-}
 putchar(' ');
+}
 }
 return (0);
 }
-
