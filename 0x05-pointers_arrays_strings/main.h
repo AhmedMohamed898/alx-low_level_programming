c4383cd5672f26e64008e0356f04d3ac5bb5ab9a
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-24 16:31:12 -0700

    678

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char c);
 int _putchar(char c);
-
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
