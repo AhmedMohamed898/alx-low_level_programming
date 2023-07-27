51e38c4d7b9a7d0143fd2ea88d5cd4bb340d3da9
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-27 14:17:36 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - check the code
+ *@dest: parameter
+ *@src: paramter
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+dest = strcat(dest, src);
+}
