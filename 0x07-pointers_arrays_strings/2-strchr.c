29208ef499cf4ea4105604c9539803375a7b20c1
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-07-31 08:04:04 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,19 @@
-#include <stddef.h>
+
 #include "main.h"
 /**
- * _strchr - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @c: the desired value
- *
- * Return: changed array with new value for n bytes
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-int size = sizeof(s);
-for (; i < size; i++)
-{
-if (s[i] == c)
-{
-return (s + i);
-}
-}
-if (i >= size)
-{
-return (NULL);
-}
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
