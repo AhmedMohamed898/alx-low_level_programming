f3d94ecb7d0018447b3a29a5a2f4676fd6160ff8
Ahmed Gomaa <ahmedmohamed898@gmail.com>
2023-08-16 05:16:30 -0700

    1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,11 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* EMPTY_HEADER_H */
 
